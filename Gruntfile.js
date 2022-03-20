94ec73cfb205f11cd61ac96b6e1173f278b4f658
seb86 <mailme@sebastiendumont.com>
2022-03-20 06:09:20 +0100

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,286 @@
+/**
+ * Build automation scripts.
+ * 
+ * @package CoCart
+ */
+
+module.exports = function(grunt) {
+	'use strict';
+
+	var sass = require( 'node-sass' );
+
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Setting folder templates.
+		dirs: {
+			css: 'assets/css',
+			scss: 'src/scss',
+			js: 'assets/js',
+		},
+
+		// Update developer dependencies
+		devUpdate: {
+			packages: {
+				options: {
+					packageJson: null,
+					packages: {
+						devDependencies: true,
+						dependencies: false
+					},
+					reportOnlyPkgs: [],
+					reportUpdated: false,
+					semver: true,
+					updateType: 'force'
+				}
+			}
+		},
+
+		// Minify JavaScript
+		uglify: {
+			options: {
+				compress: {
+					global_defs: {
+						"EO_SCRIPT_DEBUG": false
+					},
+					dead_code: true
+				},
+				banner: '/*! <%= pkg.title %> v<%= pkg.version %> <%= grunt.template.today("dddd dS mmmm yyyy HH:MM:ss TT Z") %> */'
+			},
+			build: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/admin',
+					src: [
+						'*.js',
+						'!*.min.js'
+					],
+					dest: '<%= dirs.js %>/admin',
+					ext: '.min.js'
+				}]
+			}
+		},
+
+		// Check for Javascript errors.
+		jshint: {
+			options: {
+				reporter: require('jshint-stylish'),
+				globals: {
+					"EO_SCRIPT_DEBUG": false,
+				},
+				'-W099': true, // Mixed spaces and tabs
+				'-W083': true, // Fix functions within loop
+				'-W082': true, // Declarations should not be placed in blocks
+				'-W020': true, // Read only - error when assigning EO_SCRIPT_DEBUG a value.
+			},
+			all: [
+				'<%= dirs.js %>/admin/*.js',
+				'!<%= dirs.js %>/admin/*.min.js'
+			]
+		},
+
+		// SASS to CSS
+		sass: {
+			options: {
+				implementation: sass,
+				sourcemap: 'none'
+			},
+			dist: {
+				files: {
+					'<%= dirs.css %>/admin/cocart.css' : '<%= dirs.scss %>/admin/admin.scss',
+					'<%= dirs.css %>/admin/cocart-setup.css' : '<%= dirs.scss %>/admin/cocart-setup.scss',
+					'<%= dirs.css %>/admin/plugin-search.css' : '<%= dirs.scss %>/admin/plugin-search.scss'
+				}
+			}
+		},
+
+		// Generate RTL .css files.
+		rtlcss: {
+			dist: {
+				expand: true,
+				src: [
+					'<%= dirs.css %>/admin/*.css',
+					'!<%= dirs.css %>/admin/*-rtl.css',
+					'!<%= dirs.css %>/admin/*.min.css'
+				],
+				ext: '-rtl.css'
+			}
+		},
+
+		// Post CSS
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer')
+				]
+			},
+			dist: {
+				expand: true,
+				src: [
+					'!<%= dirs.css %>/admin/*.min.css',
+					'<%= dirs.css %>/admin/*.css'
+				],
+				ext: '.css'
+			}
+		},
+
+		// Minify CSS
+		cssmin: {
+			options: {
+				processImport: false,
+				roundingPrecision: -1,
+				shorthandCompacting: false
+			},
+			target: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.css %>/admin',
+					src: [
+						'*.css',
+						'!*.min.css'
+					],
+					dest: '<%= dirs.css %>/admin',
+					ext: '.min.css'
+				}]
+			}
+		},
+
+		// Watch for changes made.
+		watch: {
+			postcss: {
+				files: [
+					'!<%= dirs.css %>/admin/*.min.css',
+					'<%= dirs.css %>/admin/*.css'
+				],
+				tasks: ['postcss'],
+				options: {
+					interrupt: true
+				}
+			},
+			css: {
+				files: [
+					'<%= dirs.scss %>/*.scss',
+					'<%= dirs.scss %>/admin/*.scss',
+				],
+				tasks: ['css']
+			},
+			js: {
+				files: [
+					'<%= dirs.js %>/admin/*.js',
+					'!<%= dirs.js %>/admin/*.min.js'
+				],
+				tasks: ['jshint']
+			},
+		},
+
+		// Check for Sass errors with "stylelint"
+		stylelint: {
+			options: {
+				configFile: '.stylelintrc'
+			},
+			all: [
+				'<%= dirs.scss %>/**/*.scss',
+			]
+		},
+
+		// Bump version numbers (replace with version in package.json)
+		replace: {
+			package: {
+				src: [
+					'load-package.php',
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /@version .*$/m,
+						to: "@version <%= pkg.version %>"
+					},
+				]
+			}
+		},
+
+		// Copies the plugin to create deployable plugin.
+		copy: {
+			build: {
+				files: [
+					{
+						expand: true,
+						src: [
+							'**',
+							'!.*',
+							'!**/*.{gif,jpg,jpeg,js,json,log,lock,md,png,scss,sh,txt,xml,zip}',
+							'!.*/**',
+							'!.DS_Store',
+							'!.htaccess',
+							'!<%= pkg.name %>-git/**',
+							'!<%= pkg.name %>-svn/**',
+							'!node_modules/**',
+							'!releases/**',
+							'!vendor/**',
+							'!unit-tests/**',
+							'readme.txt'
+						],
+						dest: 'build/',
+						dot: true
+					}
+				]
+			}
+		},
+
+		// Compresses the package folder.
+		compress: {
+			zip: {
+				options: {
+					archive: './releases/<%= pkg.name %>-v<%= pkg.version %>.zip',
+					mode: 'zip'
+				},
+				files: [
+					{
+						expand: true,
+						cwd: './build/',
+						src: '**',
+						dest: '<%= pkg.name %>'
+					}
+				]
+			}
+		},
+
+		// Deletes the build folder once zipped up.
+		clean: {
+			build: [ 'build/' ]
+		}
+	});
+
+	// Set the default grunt command to run test cases.
+	grunt.registerTask( 'default', [ 'test' ] );
+
+	// Checks for developer dependencies updates.
+	grunt.registerTask( 'check', [ 'devUpdate' ] );
+
+	// Checks for errors.
+	grunt.registerTask( 'test', [ 'stylelint', 'jshint' ] );
+
+	// Build CSS ONLY!
+	grunt.registerTask( 'css', [ 'stylelint', 'sass', 'rtlcss', 'postcss', 'cssmin' ] );
+
+	// Build JS ONLY!
+	grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
+
+	// Update version of plugin.
+	grunt.registerTask( 'version', [ 'replace:package' ] );
+
+	// Zips up package.
+	grunt.registerTask( 'zip', [ 'copy:build', 'compress:zip', 'clean:build' ] );
+
+	// Build Plugin.
+	grunt.registerTask( 'build', [ 'version', 'css', 'js', 'zip' ] );
+
+	// Register Watcher Tasks.
+	grunt.registerTask( 'watch-css', ['watch:css'] );
+	grunt.registerTask( 'watch-postcss', ['watch:postcss'] );
+	grunt.registerTask( 'watch-js', ['watch:js'] );
+
+};
---
+++ load-package.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is designed to be used to load as package NOT a WP plugin!
+ *
+ * @version 1.0.0
+ * @package CoCart Admin Package
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'COCART_ADMIN_PACKAGE_FILE' ) ) {
+	define( 'COCART_ADMIN_PACKAGE_FILE', __FILE__ );
+}
+
+// Include the main CoCart Admin Package class.
+if ( ! class_exists( 'CoCart\Admin\Package', false ) ) {
+	include_once( untrailingslashit( plugin_dir_path( COCART_ADMIN_PACKAGE_FILE ) ) . '/includes/class-cocart-admin.php' );
+}
+
+/**
+ * Returns the main instance of cocart_admin_package and only runs if it does not already exists.
+ *
+ * @return cocart_admin_package
+ */
+if ( ! function_exists( 'cocart_admin_package' ) ) {
+	function cocart_admin_package() {
+		return \CoCart\Admin\Package::init();
+	}
+
+	cocart_admin_package();
+}
